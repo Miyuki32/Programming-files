7ec9afd29090cda39549aa7d9e9ec6688142985e
Ivan Liang Jin Ngu <104381576@students.swinburne.edu.my>
2024-03-06 07:57:52 +0800

    Added files from Lecture 2 and Activity for Week 2

---
+++ Lecture/2/lec_1.py
@@ -0,0 +1,8 @@
+#This is my first python program
+print('This is the first line')
+print('This is the second line')
+print('this is the first line \nthis is the second line')
+
+
+print('This is a test   this is a skip line')
+print('This is a test \t this is a skip line')
